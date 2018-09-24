5b8a56767a8110bfb8bed9e4d1a8aa28f67a40df
Daniel Matthes <dmatthes@cbs.mpg.de>
2018-09-24 13:42:57 +0200

    Script LI_init – Script improved → it works no properly within Linux and Windows systems Function LI_exportPowspctrm → NEW Function → exports the complete power spectrum of a certain dataset to a excel spreadsheet at a selected location.

--- LI_init.m
+++ LI_init.m
@@ -3,7 +3,16 @@
 % -------------------------------------------------------------------------
 
 filepath = fileparts(mfilename('fullpath'));
-addpath(sprintf('%s/:%s/easyplot:%s/functions:%s/utilities', ...
-        filepath, filepath, filepath, filepath));
+
+if strcmp(computer, 'GLNXA64')
+  addpath(sprintf('%s/:%s/easyplot:%s/functions:%s/utilities:%s/export',...
+          filepath, filepath, filepath, filepath, filepath));
+elseif strcmp(computer, 'PCWIN64')
+  addpath(filepath);
+  addpath([filepath '\easyplot']);
+  addpath([filepath '\functions']);
+  addpath([filepath '\utilities']);
+  addpath([filepath '\export']);
+end
 
 clear filepath
---
+++ export/LI_exportPowspctrm.m
@@ -0,0 +1,40 @@
+function LI_exportPowspctrm( data )
+% LI_EXPORTPOWSPCTRM exports power spectrum tables into excel spreadsheets.
+%
+% Use as
+%   LI_exportPowspctrm( data )
+%
+% where the input data have to be a result of LI_PSD.
+%
+% This function requires the fieldtrip toolbox
+%
+% See also LI_PSD
+
+% Copyright (C) 2018, Daniel Matthes, MPI CBS
+
+% -------------------------------------------------------------------------
+% Get and check input data
+% -------------------------------------------------------------------------
+powspctrm   = ft_getopt(data, 'powspctrm', []);
+
+if isempty(powspctrm)
+  error('Wrong dataset! The selected one has no field powspctrm');
+end
+
+% -------------------------------------------------------------------------
+% Select file destination
+% -------------------------------------------------------------------------
+[file, filepath] = uiputfile('powspctrm.xls');
+filepath = [filepath file];
+
+% -------------------------------------------------------------------------
+% Create and save table
+% -------------------------------------------------------------------------
+powspctrm = num2cell(data.powspctrm');
+freq      = num2cell(data.freq');
+table     = [freq powspctrm];
+table     = cell2table(table);
+table.Properties.VariableNames =  [{'freq'} data.label'];
+writetable(table, filepath);
+
+end
